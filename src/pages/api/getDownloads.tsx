0e0ca2873c0dea7e0608e3d27d8dfc17b6d13181
Paul Buscano <pbuscano@deloitte.com>
2023-02-22 10:50:53 +0800

    initial commit

---
+++ src/pages/api/getDownloads.tsx
@@ -0,0 +1,21 @@
+import { JSDOM } from "jsdom";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const getDownloads = async (req: NextApiRequest, res: NextApiResponse) => {
+  const body = JSON.parse(req.body);
+  const { inputField } = body;
+
+  const response = await fetch(
+    `https://www.npmjs.com/package/${inputField.toLowerCase()}`
+  );
+  const html = await response.text();
+
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  const downloads = document.querySelector("._9ba9a726")?.textContent;
+
+  res.status(200).json({ downloads });
+};
+
+export default getDownloads;
